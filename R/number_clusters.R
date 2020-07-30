42f787628fdbd50c1161fb2cc416f50e31a9162d
Fanqian Yin <fanqianyin@outlook.com>
2020-07-30 12:13:01 +0800

    add function cluster_number()

--- R/number_clusters.R
+++ R/number_clusters.R
@@ -1,33 +1,26 @@
-
-
-
-
-
 #' cluster_number
 #'
 #' Produce one referenced number of clusters which determined by kmean/pam/clara/fanny/hcut clustering.
 #'
-#' @param Exp data.frame with rownames represent samples, colnames represent features like proteins or genes.
+#' @param Exp data.frame or matrix with rownames represent samples, colnames represent features like proteins or genes.
 #' @param k.max maximum numbers of cluster.
-#' @param FUNcluster Clustering method
+#' @param FUNcluster Clustering method. Type without quote mark. Chose one of following methods: kmean, pam, clara, fanny, hcut.
 #' @param scale Logical. whether input Exp should be scaled.
+#' @param ... Any parameter passed to fviz_nbclust().
 #'
-#' @return Two plots. Indicating number of clusters.
-#' @export
+#' @return Two plots. Indicating number of clusters by using Elbow method and Silhouette method.
 #'
 #' @examples
 #' cluster_number(iris[-5], 5, kmeans)
 #'
+#' @seealso \code{\link[cluster]{fviz_nbclust}}
+#'
+#' @export
 
-
-
-cluster_number <- function(Exp, k.max, FUNcluster = c(kmeans, pam, clara, fanny, hcut), scale = TRUE){
+cluster_number <- function(Exp, k.max, FUNcluster = c(kmeans, pam, clara, fanny, hcut), scale = TRUE, ...){
   d <- as.matrix(Exp)
   if (scale == TRUE) d <- scale(d)
-  require(factoextra)
-  require(cluster)
-  p1 <- fviz_nbclust(d, FUNcluster, method = "wss")+labs(subtitle = "Elbow method")
-  p2 <- fviz_nbclust(d, FUNcluster, method = "silhouette")+labs(subtitle = "Silhouette method")
-  require(cowplot)
-  plot_grid(p1,p2)
+  p1 <- fviz_nbclust(d, FUNcluster, method = "wss", ...)+labs(subtitle = "Elbow method")
+  p2 <- fviz_nbclust(d, FUNcluster, method = "silhouette", ...)+labs(subtitle = "Silhouette method")
+  cowplot::plot_grid(p1,p2)
 }
