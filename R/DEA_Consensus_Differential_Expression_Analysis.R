e4583f291a6630d009948d79e0418e73039da98b
Fanqian Yin <fanqianyin@outlook.com>
2020-11-10 16:56:13 +0800

    update

--- R/DEA_Consensus_Differential_Expression_Analysis.R
+++ R/DEA_Consensus_Differential_Expression_Analysis.R
@@ -16,18 +16,40 @@
 #'
 #' @examples
 #' res.anova <- DEA_ANOVA(iris[-5], groups = iris$Species)
-Consensus_Differential_Expression <- function(Exp, groups, adj.method = "BH"){
+#' res.ttest <- DEA_t.test(exp, groups)
+Consensus_Differential_Expression <- function(Exp, groups, methods = "ALL", adj.method = "BH", fearture.name = "gene", center.fun = "mean"){
 
 }
 
+#' @export
 #t.test
-DEA_t.test <- function(Exp, groups, adj.method = "BH"){
+DEA_t.test <- function(Exp, groups, adj.method = "BH", fearture.name = "gene", center.fun = "mean"){
+  Exp <- as.matrix(Exp)
+  res.df <- data.frame("variale" = colnames(Exp))
+
+  p.value <- apply(Exp, 2, function(x) {
+    #fo <- as.formula("x", " ~ ", "group")
+    t.test.result <- t.test(x[groups==levels(groups)[1]],x[groups==levels(groups)[2]])
+    p <- t.test.result$p.value
+    return(p)
+  })
+  p.value <- as.vector(p.value)
+  res.df <- cbind(res.df,p.value)
+  colnames(res.df) <- c(fearture.name, "p")
+  #p.adjust
+  res.df <- res.df %>% mutate("p.adj" = p.adjust(p, method = adj.method))
+  #add other descriptive statistics
+
+
+  res.fc <- calculate_FC(Exp, groups, group.number = 2, fun = center.fun, fearture.name = fearture.name)
+  res.df <- dplyr::left_join(res.df, res.fc) %>% dplyr::arrange(p.adj)
+  return(res.df)
 
 }
 
 #' @export
 #one-way analysis of variance (ANOVA), parametric, >2 groups.
-DEA_ANOVA <- function(Exp, groups, type = "one-way", adj.method = "BH", fearture.name = "gene", center.fun = "mean"){
+DEA_ANOVA <- function(Exp, groups, type = "one-way", group.number = 3, adj.method = "BH", fearture.name = "gene", center.fun = "mean"){
   Exp <- as.data.frame(Exp)
   res.df <- data.frame()
   if (type == "one-way") {
@@ -53,6 +75,11 @@ DEA_ANOVA <- function(Exp, groups, type = "one-way", adj.method = "BH", fearture
     res.df <- dplyr::left_join(res.df, res.fc)
     return(res.df)
   }
+}
+
+#limma imbeded test method
+DEA_limma.test <- function(Exp, groups, adj.method = "BH", fearture.name = "gene", center.fun = "mean"){
+
 }
 
 #Kruskal-Wallis test, non-parametric, >2 groups.
@@ -93,8 +120,7 @@ calculate_mean <- function(data, groups, fun = "mean", fearture.name = "gene"){
   }
 }
 #return groups of mean, FC, log2(FC)
-calculate_FC <- function(data, groups, fun = "mean", fearture.name = "gene"){
-  #mean
+calculate_FC <- function(data, groups, group.number, fun = "mean", fearture.name = "gene"){
   if (fun == "mean") {
     res.means <- calculate_mean(data, groups, fun, fearture.name)
     name.groups <- colnames(res.means)[-1];name.groups <- stringr::str_sub(name.groups, 6) #get group names
@@ -103,7 +129,16 @@ calculate_FC <- function(data, groups, fun = "mean", fearture.name = "gene"){
     name.groups <- colnames(res.means)[-1];name.groups <- stringr::str_sub(name.groups, 8) #get group names
   }else{
       stop("fun should be either mean or median")
-    }
+  }
+
+  if(group.number == 2){
+    res.FCs <- as.data.frame(res.means)
+    res.FC <- res.FCs[[2]]/res.FCs[[3]]
+    res.FC.log <- log2(res.FC)
+    res <- cbind(res.FCs, res.FC, res.FC.log)
+    colnames(res) <- c(fearture.name, name.groups, "FC", "log2FC")
+
+  }else if(group.number > 2){
     res.means.l <- res.means[-1]
     levs <- levels(as.factor(groups))
     res.index <- pairwise.compare.index(groups)
@@ -118,6 +153,8 @@ calculate_FC <- function(data, groups, fun = "mean", fearture.name = "gene"){
       res.FCs <- cbind(res.FCs, res.FC, res.FC.log)
     }
     res <- dplyr::left_join(res.means, res.FCs)
+  }
+
   return(res)
 }
 
